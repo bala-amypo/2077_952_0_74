5c62cfc41fe31efcf09c3d2cb874020217df758b
bala-amypo <bala@amypo.in>
2025-12-19 05:09:23 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 @ExceptionHandler(MethodArgumentNotValidException.class)
-public 
+public ResponseEntity <? > hello(MethodArgumentNotValidException ex) {
+Map<String, String> map=new HashMap<>();
+}
+@ExceptionHandler(PasswordhotwatchCri.class)
+public String hi(){
+return "Hi";
            
 }
