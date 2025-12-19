8940d626825fbcbe810ddf9c2be4c229df4c240b
bala-amypo <bala@amypo.in>
2025-12-19 04:21:24 +0000

    User commit

--- src/main/java/com/example/demo/exception/PasswordNotMatchCri.java
+++ src/main/java/com/example/demo/exception/PasswordNotMatchCri.java
@@ -1,4 +1,10 @@
 package com.example.demo.exception;
 public class PasswordNotMatchCri extends RuntimeException{
-    
+    public PasswordNotMatchCri(String msg){
+        super(msg);
+    }
+}
+
+if(pass.length()<8){
+    throw new PasswordNotMatchCri("Password should have ")
 }
